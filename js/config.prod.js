97f355c75c43ac67cf83c1acc156c19903c46e99
F4IRUZZZ <221679966+F4IRUZZZ@users.noreply.github.com>
2026-09-22 20:18:43 +0700

    config: URL backend produksi Back4app

--- js/config.prod.js
+++ js/config.prod.js
@@ -2,4 +2,4 @@
 // Aman di-commit: URL backend publik memang harus terlihat browser;
 // yang melindungi data = token login + CORS, bukan kerahasiaan URL.
 // Ganti dengan URL Back4app-mu (lihat PANDUAN-HOSTING.md langkah 2).
-window.API_BASE = 'https://keuangan-api-XXXX.back4app.app';
+window.API_BASE = 'https://keuanganapi1-82zh4kgb.b4a.run';
